651924ecc7010c3d479767b2cf182e4ce91b30ee
russellandrady <russellsuper2000@gmail.com>
2024-04-10 15:13:39 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs";//github copilot gives only bcrypt. But we need bcryptjs
+import bcryptjs from "bcryptjs"; //github copilot gives only bcrypt. But we need bcryptjs
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,7 +15,33 @@ export const signup = async (req, res, next) => {
     await newUser.save(); //by await, we say js to stay here until this is done and go next line. To use await function should be asynchrnous
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    next(error);//this calls the error handling middleware in index.js
+    next(error); //this calls the error handling middleware in index.js
     //next(errorHandler(5000, "Something gone wrong"));//this can use instead the previous line to make an custom error
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email }); //with findone we can find the first user with the email
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = await bcryptjs.compare(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong credentials"));
+    }
+    const { password: pass, ...rest } = validUser._doc; //we destructure the password from the user object and the rest. This _doc remove the unneccesary things
+    const token = jwt.sign(
+      { id: validUser._id, email: validUser.email },
+      process.env.JWT_SECRET
+    );
+    let expiryDate = new Date(Date.now() + 60 * 60 * 1000); // 1 hour from now
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest); //we create a cookie named access_token with the token in client's browser that can only access by http. Not by third party apps. And finally we send the validUser credentials to the client
+  } catch (error) {
+    next(error);
   }
 };
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {//each time we go /api we gonna use localhost 3000 instead of 5173
+        target: 'http://localhost:3000',
+        secure: false,
+      }
+
+    },
+  },
+});
