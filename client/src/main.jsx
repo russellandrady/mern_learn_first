593d06152c4c6a8858fbb106efce827841dfecd1
russellandrady <russellsuper2000@gmail.com>
2024-04-11 12:49:48 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {store} from './redux/store.js'//we are importing the store from redux to provide it all over the app. 
+import {persistor, store} from './redux/store.js'//we are importing the store from redux to provide it all over the app. 
 import {Provider} from 'react-redux'//we are importing the provider from react-redux to provide the store to all the components.
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      </PersistGate>
     <App />
   </Provider>,
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers,configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";//actually this is reducer in the userSlice. But we rename it here to userReducer. 
+import { persistReducer, persistStore} from "redux-persist";
+import { version } from "mongoose";
+import storage from "redux-persist/lib/storage";//this is used to store the data on the local storage.
+const rootReducer = combineReducers({user:userReducer});//we are combining the reducers here.
+
+const persistConfig = {
+  key: "root",//this is just a name for storing data on the local storage.
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);//we are persisting the reducer here. Persisting helps to keep the user data even after refreshing the pages by the user. Unless the user logs out, the data will be there.
+
 export const store = configureStore({
-  reducer: {user:userReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+ export const persistor = persistStore(store);//this is used to persist the store.
