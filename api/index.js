cbdfd79d22762ae525613ce937c94e6e616f7aad
russellandrady <russellsuper2000@gmail.com>
2024-04-14 09:38:22 +0530

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'; //userRoutes name is not there is user.route.js file. We defined it here in index.js
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -12,6 +13,11 @@ mongoose.connect(process.env.MONGO).then(() => {
   console.log(err)
 });
 const app = express()
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 app.use(express.json());
 app.use(cookieParser());
 app.listen(3000, () => {
